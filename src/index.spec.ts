10a077d92382042d69ee2a75e84ce239b46a1bfc
Ovidiu Barabula <ovidiu@graffino.com>
2018-03-12 15:41:53 +0200

    feat(core): instantiate module with ConfigManager

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,6 @@
 import { expect } from 'chai';
 import 'mocha';
-import Greeter from './index';
 
-describe('Greeter', () => {
-    it('correctly creates greeter instance', () => {
-        const greeter = Greeter('Hola');
-        expect(greeter.hello('John')).to.equal('Hola, John!');
-    });
-
-    it('throws if instantiated without parameter', () => {
-        expect(() => Greeter()).to.throw(Error);
-    });
+describe('Frontvue', () => {
+  it('instantiates');
 });
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,9 @@
  * @since 0.1.0
  */
 
-interface IGreeting {
-  hello(name: string): string;
-}
+import ConfigManagerFactory from './config-manager/index';
 
-const GreetingFactory = function (greeting: string): IGreeting {
-  if (!greeting || typeof greeting !== 'string') {
-    throw new Error(`GreetingFactory requires 1 parameter to be a string, ${typeof greeting} was passed instead`);
-  }
-
-  return Object.freeze({
-    hello(name: string): string {
-      return `${greeting}, ${name}!`;
-    },
-  });
-};
-
-export default GreetingFactory;
+(async function frontvue() {
+  const name = 'frontvue';
+  const configManager = await ConfigManagerFactory(name);
+}());
