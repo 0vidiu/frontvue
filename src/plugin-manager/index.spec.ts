44c4ed9984c6b4bc0f4f6defca69bb26d2b207df
Ovidiu Barabula <ovidiu@graffino.com>
2018-03-31 14:58:20 +0300

    feat(core): remove plugin validation responsability from plugin manager and use installable

--- src/plugin-manager/index.spec.ts
+++ src/plugin-manager/index.spec.ts
@@ -1,18 +1,28 @@
 import { assert, expect } from 'chai';
 import 'mocha';
+import ConfigManager from '../config-manager';
+import ConfigWizard from '../config-wizard';
 import TaskManager from '../task-manager';
 import PluginManager, { ERRORS } from './index';
 
 describe('PluginManager', () => {
-  const taskManager = TaskManager();
-  const validPlugin = {
-    install: () => true,
-  };
+  let taskManager;
+  let configWizard;
+  let validPlugin;
+
+  beforeEach(async () => {
+    taskManager = TaskManager({
+      hooks: ['before', 'midway', 'after'],
+    });
+    configWizard = ConfigWizard(await ConfigManager());
+    validPlugin = { install: () => true };
+  });
+
 
   it('instantiates', () => {
-    const pluginManager = PluginManager(taskManager);
+    const pluginManager = PluginManager(taskManager, configWizard);
     expect(pluginManager).to.be.an('object')
-      .to.contain.keys('use', 'validate');
+      .to.contain.keys('use');
   });
 
 
@@ -23,24 +33,54 @@ describe('PluginManager', () => {
   });
 
 
-  it('throws if plugin is undefined or not an object', () => {
-    assert.throws(() => PluginManager(taskManager).validate(undefined), ERRORS.PLUGIN_INVALID);
+  it('throws if not passed ConfigWizard instance', () => {
+    assert.throws(() => PluginManager(taskManager), ERRORS.NO_CONFIG_WIZARD);
+    assert.throws(() => PluginManager(taskManager, {}), ERRORS.NO_CONFIG_WIZARD);
+    assert.throws(() => PluginManager(taskManager, 1), ERRORS.NO_CONFIG_WIZARD);
   });
 
 
-  it('throws if plugin doesn\'t have .install() method', () => {
-    assert.throws(() => PluginManager(taskManager).validate({}), ERRORS.PLUGIN_NOT_INSTALLABLE);
-  });
-
-
-  it('calls taskManager.add() method if plugin is valid', () => {
+  it('calls taskManager.getSubscribers() method if plugin is valid', () => {
     let called = false;
-    const pluginManager = PluginManager({
-      add: () => called = true,
-      run: () => undefined,
-    });
 
+    const taskManagerStub = {
+      add: () => undefined,
+      getSubscribers: () => called = true,
+      run: () => undefined,
+    };
+    const pluginManager = PluginManager(taskManagerStub, configWizard);
     pluginManager.use(validPlugin);
     expect(called).to.be.true;
+  });
+
+
+  it('calls configWizard.getSubscriber() method if plugin is valid', () => {
+    let called = false;
+
+    const configManagerStub = {
+      addQuestionnaire: () => undefined,
+      getSubscriber: () => called = true,
+      start: () => undefined,
+    };
+
+    const pluginManager = PluginManager(taskManager, configManagerStub);
+    pluginManager.use(validPlugin);
+    expect(called).to.be.true;
+  });
+
+
+  it('passes taskSubscribers and configSubscriber arguments to plugin.install() method', () => {
+    const pluginStub = {
+      install(taskSubscribers, configSubscriber) {
+        expect(taskSubscribers)
+          .to.be.be.an('object')
+          .to.not.be.empty;
+        expect(configSubscriber)
+          .to.be.be.a('function');
+      },
+    };
+
+    const pluginManager = PluginManager(taskManager, configWizard);
+    pluginManager.use(pluginStub);
   });
 });
--- src/plugin-manager/index.ts
+++ src/plugin-manager/index.ts
@@ -5,54 +5,47 @@
  * @since 0.1.0
  */
 
+import { IConfigWizard, QuestionnaireSubscriber } from '../config-wizard';
 import { TaskManager, TaskSubscriber } from '../task-manager';
-
+import Installable, { InstallableObject } from './installable';
 
 export interface Plugin {
-  install(subscribers: TaskSubscriber): void;
+  name: string;
+  description?: string;
+  install(taskSubscribers: TaskSubscriber, configSubscriber: QuestionnaireSubscriber): Promise<void>;
 }
 
 export interface PluginManager {
-  use(plugin: Plugin | string): void;
+  use(plugin: Plugin | InstallableObject): Promise<void>;
   validate?(plugin: Plugin): boolean;
 }
 
 
 // Custom error messages
 export const ERRORS = {
-  NO_TASK_MANAGER: 'PluginManager requires first argument to be a TaskManager instance',
-  PLUGIN_INVALID: 'PluginManager> Invalid plugin: should be an object',
-  PLUGIN_NOT_INSTALLABLE: 'PluginManager> Invalid plugin: .install() method is missing',
+  NO_CONFIG_WIZARD: 'PluginManager() requires second argument to be a ConfigWizard instance',
+  NO_TASK_MANAGER: 'PluginManager() requires first argument to be a TaskManager instance',
 };
 
 
 /**
  * PluginManager constructor
  */
-function PluginManager(taskManager: TaskManager): PluginManager {
+function PluginManager(taskManager: TaskManager, configWizard: IConfigWizard): PluginManager {
   if (
     typeof taskManager === 'undefined' ||
     typeof taskManager !== 'object' ||
-    !taskManager.hasOwnProperty('add')
+    !taskManager.hasOwnProperty('getSubscribers')
   ) {
     throw new Error(ERRORS.NO_TASK_MANAGER);
   }
 
-
-  /**
-   * Validate plugin
-   * @param plugin Plugin object
-   */
-  function validate(plugin: Plugin): boolean {
-    if (typeof plugin === 'undefined' || typeof plugin !== 'object') {
-      throw new Error(ERRORS.PLUGIN_INVALID);
-    }
-
-    if (!plugin.hasOwnProperty('install') || typeof plugin.install !== 'function') {
-      throw new Error(ERRORS.PLUGIN_NOT_INSTALLABLE);
-    }
-
-    return true;
+  if (
+    typeof configWizard === 'undefined' ||
+    typeof configWizard !== 'object' ||
+    !configWizard.hasOwnProperty('getSubscriber')
+  ) {
+    throw new Error(ERRORS.NO_CONFIG_WIZARD);
   }
 
 
@@ -60,27 +53,30 @@ function PluginManager(taskManager: TaskManager): PluginManager {
    * Register plugin
    * @param plugin Plugin object
    */
-  function use(plugin: Plugin): void {
-    validate(plugin);
-    taskManager.add(plugin);
+  async function use(plugin: Plugin | InstallableObject): Promise<void> {
+    // TODO: Add support for plugin as string
+    // TODO: When plugin is of type string, look for plugin in node_modules
+
+    // Get task subscribers
+    const taskSubscribers: TaskSubscriber = taskManager.getSubscribers();
+    // Get config wizard questionnaire subscriber
+    const configSubscriber: QuestionnaireSubscriber = configWizard.getSubscriber();
+
+    await Installable(plugin)
+      .install(
+        // Provide the plugin with the following:
+        // Subscribers object for task hook subscription
+        taskSubscribers,
+        // Config Wizard questionnaire registrar
+        configSubscriber,
+      );
   }
 
 
-  // Creating the public API object
-  let publicApi: PluginManager = {
+  // Return public API
+  return Object.freeze({
     use,
-  };
-
-
-  // Adding private methods to public API in test environment
-  /* test:start */
-  publicApi = {...publicApi,
-    validate,
-  };
-  /* test:end */
-
-
-  return Object.freeze(publicApi);
+  });
 }
 
 export default PluginManager;
