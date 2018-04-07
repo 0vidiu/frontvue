7a360a93f26bcaf27dcd690712276d3b2003e89e
Ovidiu Barabula <ovidiu@graffino.com>
2018-04-07 17:19:21 +0300

    feat(core): add config manager proxy for plugin configuration
    Restrict access to configuration by filtering config object keys by plugin prefix. All options set
    via the proxy config manager will be set under the plugin's prefix (e.g.
    "plugin-myplugin:option-name").

---
+++ src/config-manager/config-manager-proxy.spec.ts
@@ -0,0 +1,284 @@
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+
+import { assert, expect } from 'chai';
+import 'mocha';
+import { pluginPrefix } from '../util/utility-functions';
+import ConfigManagerProxy, { ERRORS } from './config-manager-proxy';
+import ConfigManager, { Config, IConfigManager } from './index';
+import { ConfigReader } from './package-json-config-reader';
+
+
+describe('ConfigManagerProxy', () => {
+  const customReader = (namespace: string) => {
+    let storedConfig: Config = {};
+    return Object.freeze({
+      destroy(): Promise<Config|Error> {
+        const oldConfig = storedConfig;
+        delete storedConfig[namespace];
+        return Promise.resolve(oldConfig);
+      },
+      fetch(): Promise<Config|Error> {
+        return Promise.resolve(storedConfig);
+      },
+      update(config: Config): Promise<boolean|Error> {
+        storedConfig = {...config};
+        return Promise.resolve(true);
+      },
+    });
+  };
+
+
+  describe('Instance', () => {
+    let configManager: IConfigManager;
+
+
+    beforeEach(async () => {
+      configManager = await ConfigManager('frontvue', customReader);
+    });
+
+
+    it('instantiates', () => {
+      expect(ConfigManagerProxy(configManager, 'myplugin')).to.be.an('object')
+        .to.contain.keys('get', 'has', 'remove', 'set');
+    });
+
+
+    it('throws if ConfigManager instance is not passed in', () => {
+      assert.throws(() => ConfigManagerProxy(), ERRORS.NO_CONFIG_MANAGER);
+    });
+
+
+    it('throws if prefix is not passed in', () => {
+      assert.throws(() => ConfigManagerProxy(configManager), ERRORS.NO_PREFIX);
+    });
+  });
+
+
+  describe('method has()', () => {
+    let configManager: IConfigManager;
+    let configProxy: IConfigManager;
+
+
+    beforeEach(async () => {
+      configManager = await ConfigManager('frontvue', customReader);
+      configProxy = ConfigManagerProxy(configManager, 'myplugin');
+    });
+
+
+    it('returns true if option is found', async () => {
+      await configManager.set(`${pluginPrefix('myplugin')}option`, 'value');
+      return expect(configProxy.has('option')).to.eventually.be.true;
+    });
+
+
+    it('returns false if option is not found', () => {
+      return expect(configProxy.has('option')).to.eventually.be.false;
+    });
+
+
+    it('returns false if option exists, but it\'s without prefix', async () => {
+      await configManager.set('option', 'value');
+      return expect(configProxy.has('option')).to.eventually.be.false;
+    });
+
+
+    it('throws if passed parameter is not a string', () => {
+      return expect(configProxy.has(1)).to.be.rejectedWith(ERRORS.KEY_MUST_BE_STRING);
+    });
+  });
+
+
+  describe('method get()', () => {
+    let configManager: IConfigManager;
+    let configProxy: IConfigManager;
+
+
+    beforeEach(async () => {
+      configManager = await ConfigManager('frontvue', customReader);
+      configProxy = ConfigManagerProxy(configManager, 'myplugin');
+    });
+
+
+    it('returns undefined if option is not found', () => {
+      return expect(configProxy.get('option')).to.eventually.be.undefined;
+    });
+
+
+    it('returns undefined if option exists, but without prefix', async () => {
+      await configManager.set('option', 'value');
+      return expect(configProxy.get('option')).to.eventually.be.undefined;
+    });
+
+
+    it('returns option value', async () => {
+      await configManager.set(`${pluginPrefix('myplugin')}option`, 'value');
+      return expect(configProxy.get('option')).to.eventually.equal('value');
+    });
+
+
+    it('returns all options if no key is passed', async () => {
+      // Set other options that should not be returned
+      await configManager.set('option1', 'value1');
+      await configManager.set('option2', 'value2');
+      await configManager.set('option3', 'value3');
+      // Set prefixed options
+      await configManager.set(`${pluginPrefix('myplugin')}option1`, 'value1');
+      await configManager.set(`${pluginPrefix('myplugin')}option2`, 'value2');
+      await configManager.set(`${pluginPrefix('myplugin')}option3`, 'value3');
+
+      return expect(configProxy.get()).to.eventually.deep.equal({
+        'plugin-myplugin:option1': 'value1',
+        'plugin-myplugin:option2': 'value2',
+        'plugin-myplugin:option3': 'value3',
+      });
+    });
+
+
+    it('returns multiple options as an object', async () => {
+      // Set prefixed options
+      await configManager.set(`${pluginPrefix('myplugin')}option1`, 'value1');
+      await configManager.set(`${pluginPrefix('myplugin')}option2`, 'value2');
+      await configManager.set(`${pluginPrefix('myplugin')}option3`, 'value3');
+
+      return expect(configProxy.get(['option1', 'option2'])).to.eventually.deep.equal({
+        'plugin-myplugin:option1': 'value1',
+        'plugin-myplugin:option2': 'value2',
+      });
+    });
+
+
+    it('returns undefined if no string, nor array is passed', async () => {
+      // Set prefixed options
+      await configManager.set(`${pluginPrefix('myplugin')}option1`, 'value1');
+      await configManager.set(`${pluginPrefix('myplugin')}option2`, 'value2');
+      await configManager.set(`${pluginPrefix('myplugin')}option3`, 'value3');
+
+      return expect(configProxy.get(['option1', 'option2'])).to.eventually.deep.equal({
+        'plugin-myplugin:option1': 'value1',
+        'plugin-myplugin:option2': 'value2',
+      });
+    });
+
+
+    it('throws if argument is not a string or array of strings', () => {
+      return expect(configProxy.get(1)).to.be.rejectedWith(ERRORS.KEY_MUST_BE_STRING);
+    });
+  });
+
+
+  describe('method set()', () => {
+    let configManager: IConfigManager;
+    let configProxy: IConfigManager;
+
+
+    beforeEach(async () => {
+      configManager = await ConfigManager('frontvue', customReader);
+      configProxy = ConfigManagerProxy(configManager, 'myplugin');
+    });
+
+
+    it('returns true when setting an option', () => {
+      return expect(configProxy.set('option', 'value')).to.eventually.be.true;
+    });
+
+
+    it('returns true when setting multiple options as an object', () => {
+      const config = {
+        option1: 'value1',
+        option2: 'value2',
+        option3: 'value3',
+      };
+
+      return expect(configProxy.set(config)).to.eventually.be.true;
+    });
+
+
+    it('sets option correctly', async () => {
+      await configProxy.set('option', 'value');
+      return expect(configManager.get()).to.eventually.deep.equal({
+        'plugin-myplugin:option': 'value',
+      });
+    });
+
+
+    it('sets multiple options correctly', async () => {
+      await configProxy.set({
+        option1: 'value1',
+        option2: 'value2',
+        option3: 'value3',
+      });
+      return expect(configManager.get()).to.eventually.deep.equal({
+        'plugin-myplugin:option1': 'value1',
+        'plugin-myplugin:option2': 'value2',
+        'plugin-myplugin:option3': 'value3',
+      });
+    });
+
+
+    it('throws if passed parameter is not a string or object', () => {
+      return expect(configProxy.set(1, 'value'))
+        .to.be.rejectedWith(ERRORS.OPTION_MUST_BE_OBJECT_OR_STRING);
+    });
+
+
+    it('throws if passed parameter is not a string or object', () => {
+      return expect(configProxy.set([]))
+        .to.be.rejectedWith(ERRORS.OPTION_MUST_BE_OBJECT_OR_STRING);
+    });
+  });
+
+
+  describe('method remove()', () => {
+    let configManager: IConfigManager;
+    let configProxy: IConfigManager;
+
+
+    beforeEach(async () => {
+      configManager = await ConfigManager('frontvue', customReader);
+      configProxy = ConfigManagerProxy(configManager, 'myplugin');
+    });
+
+
+    it('removes an option', async () => {
+      await configManager.set(`${pluginPrefix('myplugin')}option`, 'value');
+      await configProxy.remove('option');
+      return expect(configManager.get()).to.eventually.deep.equal({});
+    });
+
+
+    it('removes multiple options', async () => {
+      await configManager.set({
+        [`${pluginPrefix('myplugin')}option1`]: 'value1',
+        [`${pluginPrefix('myplugin')}option2`]: 'value2',
+        [`${pluginPrefix('myplugin')}option3`]: 'value3',
+      });
+
+      await configProxy.remove('option1', 'option2', 'option3');
+      return expect(configManager.get()).to.eventually.deep.equal({});
+    });
+
+
+    it('returns true when it removes an option', async () => {
+      await configManager.set(`${pluginPrefix('myplugin')}option`, 'value');
+      return expect(configProxy.remove('option')).to.eventually.be.true;
+    });
+
+
+    it('returns true when it removes multiple options', async () => {
+      await configManager.set({
+        [`${pluginPrefix('myplugin')}option1`]: 'value1',
+        [`${pluginPrefix('myplugin')}option2`]: 'value2',
+        [`${pluginPrefix('myplugin')}option3`]: 'value3',
+      });
+      return expect(configProxy.remove('option1', 'option2', 'option3')).to.eventually.be.true;
+    });
+
+
+    it('throws if passed parameter(s) is/are not of type string', () => {
+      return expect(configProxy.remove('option', 1, true))
+        .to.be.rejectedWith(ERRORS.KEY_MUST_BE_STRING);
+    });
+  });
+});
---
+++ src/config-manager/config-manager-proxy.ts
@@ -0,0 +1,131 @@
+/**
+ * Name: plugin-proxy.ts
+ * Description: Plugin Manager Proxy for plugins to restrict configuration access
+ * Author: Ovidiu Barabula <lectii2008@gmail.com>
+ * @since 0.1.0
+ */
+
+import { pluginPrefix, required, arrayOf } from '../util/utility-functions';
+import ConfigManager, { Config, IConfigManager } from './index';
+import ConfigPrefixer from './prefixer';
+
+
+// Custom error messages
+export const ERRORS = {
+  KEY_MUST_BE_STRING: 'ConfigManagerProxy> .get(), .has() and .remove() methods require first parameter to be of type \'string\' or \'array of strings\'',
+  NO_CONFIG_MANAGER: 'ConfigManagerProxy() requires first parameter to be an instance of ConfigManager',
+  NO_PREFIX: 'ConfigManagerProxy() requires second parameter <prefix> of type string',
+  OPTION_MUST_BE_OBJECT_OR_STRING: 'ConfigManagerProxy> .set() method requires first parameter to be of type \'string\' or an \'object\'',
+};
+
+
+/**
+ * Restrict access to configuration by allowing only prefixed keys to be read/written
+ * @param configManager Instance of ConfigManager
+ * @param prefix A configuration key prefix (e.g. plugin name)
+ */
+export default function ConfigManagerProxy(
+  configManager: IConfigManager = required(ERRORS.NO_CONFIG_MANAGER),
+  prefix: string = required(ERRORS.NO_PREFIX),
+): IConfigManager {
+  // Create configuration key plugin prefix
+  prefix = pluginPrefix(prefix);
+  // Config object prefixer instance
+  const prefixer = ConfigPrefixer(prefix);
+
+
+  /**
+   * Retrieve value from configuration
+   * @param key Configuration option key
+   */
+  async function get(key?: string | string[]): Promise<Config|any> {
+    let prefixedKey: string | string[];
+
+    // When key is not passed in we return the entire config object
+    // In this case we need to filter all the prefixed keys and return all of them
+    if (typeof key === 'undefined') {
+      // Get all options
+      const config = await configManager.get();
+      // Filter option keys and get all that begin with the prefix
+      const filteredKeys = Object.keys(config).filter(item => item.match(RegExp(`^${prefix}`)));
+      // Create new config object with the filtered key/value pairs
+      return filteredKeys.reduce((prefixedConfig, item) =>
+        ({
+          ...prefixedConfig,
+          ...{ [item]: config[item],
+        }}), {});
+    }
+
+    if (typeof key === 'string') {
+      prefixedKey = `${prefix}${key}`;
+    } else if (Array.isArray(key) && arrayOf(key, 'string')) {
+      prefixedKey = key.map(item => `${prefix}${item}`);
+    } else {
+      throw new Error(ERRORS.KEY_MUST_BE_STRING);
+    }
+
+    return await configManager.get(prefixedKey);
+  }
+
+
+  /**
+   * Check if key exists in config
+   * @param key Configuration option key
+   */
+  async function has(key: string): Promise<boolean> {
+    if (typeof key !== 'string') {
+      throw new Error(ERRORS.KEY_MUST_BE_STRING);
+    }
+
+    const prefixedKey = `${prefix}${key}`;
+    return await configManager.has(prefixedKey);
+  }
+
+
+  /**
+   * Removes one or more options from config
+   * @param keys Option name or array of option names
+   */
+  async function remove(...keys: string[]): Promise<boolean|Error> {
+    if (!arrayOf(keys, 'string')) {
+      throw new Error(ERRORS.KEY_MUST_BE_STRING);
+    }
+
+    const prefixedOptions = keys.map(item => `${prefix}${item}`);
+    return await configManager.remove(...prefixedOptions);
+  }
+
+
+  /**
+   * Set new value(s) in configuration
+   * @param option Configuration object or object key
+   * @param value New value to be set if option is an object key ("string")
+   */
+  async function set(option: Config | string, value?: any): Promise<boolean|Error> {
+    if (
+      (typeof option !== 'object' && typeof option !== 'string') ||
+      Array.isArray(option)
+    ) {
+      throw new Error(ERRORS.OPTION_MUST_BE_OBJECT_OR_STRING);
+    }
+
+    if (typeof option === 'string') {
+      return await configManager.set(`${prefix}${option}`, value);
+    }
+
+    const prefixedConfig = prefixer.apply(option);
+    return await configManager.set(prefixedConfig);
+  }
+
+
+  // Public API object
+  const publicApi: IConfigManager = {
+    get,
+    has,
+    remove,
+    set,
+  };
+
+  // Return read-only public API
+  return Object.freeze(publicApi);
+}
