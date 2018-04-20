fa7e3345fdabb197d6fa784b4e2c246807e58065
Ovidiu Barabula <ovidiu@graffino.com>
2018-04-20 16:21:19 +0300

    feat(cli): implement commands

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -8,49 +8,91 @@
 import * as program from 'commander';
 import frontvue from '../core';
 
-program
-  .version('0.1.0')
-  .arguments('<command>')
-  .action(command => {
-    if (!command) {
-      console.log('You need to type a command as well');
+// Set CLI tool version
+program.version('0.1.0', '-v, --version');
+
+
+/**
+ * Command: init
+ * Description: Initialize a new project
+ */
+program.command('init')
+  .alias('i')
+  .description('Initialize a new project')
+  .action(async () => {
+    const instance = await frontvue;
+    const logger = instance.logger('cli');
+    logger.info(`Configuring new project\u2026`);
+    await instance.run('init');
+  });
+
+
+/**
+ * Command: config
+ * Description: Start configuration wizard
+ */
+program.command('config')
+  .alias('c')
+  .description('Start configuration wizard')
+  .action(async () => {
+    const instance = await frontvue;
+    const logger = instance.logger('cli');
+    logger.info(`Configuring project\u2026`);
+    await instance.run('config');
+  });
+
+
+/**
+ * Command: dev
+ * Description: Start development mode
+ */
+program.command('dev')
+  .alias('d')
+  .description('Starts development mode')
+  .action(async () => {
+    const instance = await frontvue;
+    const logger = instance.logger('cli');
+    const hooks = ['clean', 'process', 'watch'];
+    logger.info('Starting development mode\u2026');
+
+    for (const hook of hooks) {
+      await instance.run(hook);
     }
   });
 
-program
-  .command('init [name]')
-    .action(async name => {
-      const instance = await frontvue;
-      const logger = instance.logger('cli');
-      logger.info(`Creating a new project ./${name}`);
 
-      await Promise.all(
-        [
-          instance.run('init'),
-          instance.run('template'),
-          instance.run('clean'),
-          instance.run('process'),
-          instance.run('watch'),
-        ],
-      );
-    })
-    .description('Initialize a new project');
+/**
+ * Command: build
+ * Description: Build production package
+ */
+program.command('build')
+  .alias('b')
+  .description('Build production package')
+  .action(async () => {
+    const instance = await frontvue;
+    const logger = instance.logger('cli');
+    const hooks = ['clean', 'process'];
+    logger.info('Building production package\u2026');
 
-
-program
-  .command('config')
-    .description('Start configuration wizard')
-    .action(() => console.log('Starting configuration...'));
-
-
-program
-  .command('run [hook]')
-    .description('Run tasks in a hook')
-    .action(async hook => {
-      const instance = await frontvue;
-      const logger = instance.logger('cli');
-      logger.info(`Starting ${hook}...`);
+    for (const hook of hooks) {
       await instance.run(hook);
-    });
+    }
+  });
+
+
+/**
+ * Command: run <hook>
+ * Description: Run specific tasks hook
+ */
+program.command('run <hook>')
+  .alias('r')
+  .description('Run all tasks in a specific hook sequence')
+  .action(async hook => {
+    const instance = await frontvue;
+    const logger = instance.logger('cli');
+    logger.info(`Running <${hook}> tasks\u2026`);
+    await instance.run(hook);
+  });
+
 
 program.parse(process.argv);
