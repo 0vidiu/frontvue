5ec03547fd49a81ca630bfaac9d4822dde5950e3
Ovidiu Barabula <ovidiu@graffino.com>
2018-05-10 17:51:11 +0300

    feat(core): add deps manager instance and pass to plugin manager

--- src/core.ts
+++ src/core.ts
@@ -8,12 +8,16 @@
 import ConfigManager, { IConfigManager } from './config-manager';
 import ConfigManagerProxy from './config-manager/config-manager-proxy';
 import ConfigWizard from './config-wizard';
+import depsManager from './dependencies-manager';
 import PluginManager, { PluginManager as IPluginManager } from './plugin-manager';
 import TaskManager from './task-manager';
-import taskInitProject from './tasks/task-init-project';
+import tasks from './tasks';
 import Logger, { ILogger } from './util/logger';
 
+
+// Task Manager configuration
 const taskManagerConfig = {
+  // Task hooks
   hooks: [
     'init',
     'config',
@@ -25,14 +29,20 @@ const taskManagerConfig = {
   ],
 };
 
-let internalPlugins = [
-  taskInitProject,
-];
+// Internal plugins list
+let internalPlugins = [...tasks];
 
 /* test:start */
 // Stop internal plugin loading in TEST environment
 internalPlugins = [];
 /* test:end */
+
+
+// Custom messages
+const MESSAGES = {
+  INIT_COMPLETE: 'Components initialized.',
+  INIT_COMPONENTS: 'Initializing components\u2026',
+};
 
 
 /**
@@ -49,12 +59,20 @@ export async function loadConfigPlugins(configManager: IConfigManager, pluginMan
  */
 async function Frontvue() {
   const name = 'frontvue';
+
+  // Get logger constructor
   const logger = Logger.getInstance();
+  // Create core logger instance and output message
+  const coreLogger = logger('core');
+  coreLogger.info(MESSAGES.INIT_COMPONENTS);
+
+  // Instatiate components
   const configManager = await ConfigManager(name);
   const configWizard = ConfigWizard(configManager);
   const taskManager = TaskManager(taskManagerConfig);
-  const pluginManager = PluginManager(taskManager, configWizard);
+  const pluginManager = PluginManager(taskManager, configWizard, depsManager);
   const { run } = taskManager;
+
 
   // Use internal plugin(s)
   await pluginManager.use(...internalPlugins);
@@ -67,10 +85,18 @@ async function Frontvue() {
   /* test:end */
   await loadExternalPlugins(configManager, pluginManager);
 
+
+  // Output component instatiation complete message
+  coreLogger.debug(MESSAGES.INIT_COMPLETE);
+
+
   // Return public API
   return Object.freeze({
+    // Named logger constructor
     logger,
+    // Name of the app
     name,
+    // TaskManager's run tasks method
     run,
   });
 }
